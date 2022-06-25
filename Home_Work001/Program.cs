a528192778c8378bc8a36f1f32c5478deff0b9cf
SERGY <kogo_sadgi@mail.ru>
2022-06-25 21:35:47 +0300

    .vscod

---
+++ Home_Work001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int min;
+int max;
+
+ if (num1 < num2)
+ {
+     min = num1;
+     max = num2;
+ }
+ else
+ {
+     min = num2;
+     max = num1;
+}
+Console.WriteLine("Минимальное число " + min);
+Console.WriteLine("Максимальное число " + max);
