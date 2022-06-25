1d21fc1d2f3149c9da21218b4d6703604faf2afa
SERGY <kogo_sadgi@mail.ru>
2022-06-25 21:43:06 +0300

    Home_Work003

---
+++ Home_Work003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+     Console.WriteLine(num + " - чётное число");
+}
+else
+{
+     Console.WriteLine(num + " - нечётное число");
+}
